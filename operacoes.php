549abb1d3b8746283ce8621b9cde451e57f302c9
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-01 14:54:46 -0500

    new: added file for mathematical operations and updated the document.

---
+++ operacoes.php
@@ -0,0 +1,24 @@
+<?php
+
+$idade = 21;
+$idadeDaqui10Anos = $idade + 10;
+
+echo $idadeDaqui10Anos;
+
+$idadeHa5Anos = $idade - 5;
+
+echo $idadeHa5Anos;
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+
+$doisAoCubo = 2 ** 3;
+//ou
+$doisAoCubo = 2 * 2 * 2;
+
+echo $doisAoCubo;
+
+$restoDaDivisao = 10 % 3;
+echo $restoDaDivisao;
