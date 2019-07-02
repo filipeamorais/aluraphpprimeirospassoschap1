c5bcdbb9deba981dd8601a32e83848652258cf72
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-01 20:38:17 -0500

    new: added two files for testing variable types and dealing with strings, beside the documentation and files of the official project until now

---
+++ strings.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade=21;
+
+echo "Olá mundo! Minha idade é $idade";
+
+$idade = 21;
+
+echo 'Olá mundo! Minha idade é ' . $idade;
---
+++ tipos.php
@@ -0,0 +1,18 @@
+<?php
+$idade=21;
+
+echo gettype($idade);
+
+$salario = 1000.30;
+echo gettype($salario);
+
+$divisao = 10/3;
+echo gettype($divisao);
+
+$texto = "Olá mundo!";
+echo gettype($texto);
+
+$verdadeiro = true;
+$falso = false;
+echo gettype($verdadeiro);
+echo gettype($falso);
